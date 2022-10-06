ce45e7d843451ec80f835571a06eaf7a53b6ab61
Lorimer Jenkins <90644448+LorimerJenkins@users.noreply.github.com>
2022-10-06 14:16:20 +0100

    -

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div class='background'>
       <a href='https://app.nonameprotocol.com' class='app-bttn'>App</a>
-      <h1>"No Name" Protocol</h1>
+      <h1>The "No Name" Protocol</h1>
       <p>MVP, making it easier to interact with dApps</p>
       <a href='https://twitter.com/NoNameProtocol' target='_blank'>Twitter</a>
     </div>
